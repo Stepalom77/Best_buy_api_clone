19fb67c07d65ee40c6a24e26b0e006f2c402954c
Stepalom77 <stepalom22@gmail.com>
2022-07-18 10:48:49 -0500

    docker node and msql setup

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
 FROM node:16.16.0-alpine3.16
 
-WORKDIR /Imdb_clone
+WORKDIR /best_buy_api
+
+RUN npm install i npm@latest -g
+
 COPY package*.json ./
-RUN npm install --production
+
+RUN npm install 
 
 USER node
+
 COPY . .
 
 EXPOSE 2000
 
-CMD ["node", "/Imdb_clone/app.js"]
+CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 2000;
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 2000;
 //Middlewares 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.json({ type: 'application/json' }));
 app.use(morgan('dev'));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Server up & running ✅');
