6f0838343b1a4e134b0c804d499c2b40415b73c3
Stepalom77 <stepalom22@gmail.com>
2022-07-18 21:50:07 -0500

    routes added to server

--- server.js
+++ server.js
@@ -6,11 +6,33 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 2000;
 
+//Require Routes
+const categoryRoutes = require('./server/routes/categoryRoutes');
+const departmentRoutes = require('./server/routes/departmentRoutes');
+const productRoutes = require('./server/routes/productRoutes');
+const purchaseRoutes = require('./server/routes/purchaseRoutes');
+const reviewRoutes = require('./server/routes/reviewRoutes');
+const reviewStoreRoutes = require('./server/routes/reviewStoreRoutes');
+const storeRoutes = require('./server/routes/storeRoutes');
+const subcategoryRoutes = require('./server/routes/subcategoryRoutes');
+const userRoutes = require('./server/routes/userRoutes');
+
 //Middlewares 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+
+//Routes
+app.use('/api/v1', categoryRoutes);
+app.use('/api/v1', departmentRoutes);
+app.use('/api/v1', productRoutes);
+app.use('/api/v1', purchaseRoutes);
+app.use('/api/v1', reviewRoutes);
+app.use('/api/v1', reviewStoreRoutes);
+app.use('/api/v1', storeRoutes);
+app.use('/api/v1', subcategoryRoutes);
+app.use('/api/v1', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Server up & running ✅');
