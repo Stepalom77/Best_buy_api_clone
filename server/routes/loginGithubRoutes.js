2b202daec784191272a05bf43696b7026e7a8120
Stephano Palomino <stepalom22@gmail.com>
2022-07-23 04:02:58 +0000

    fixed route of github login from droplet

--- server/routes/loginGithubRoutes.js
+++ server/routes/loginGithubRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/login/github', (req, res) => {
     const url = 
-    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=http://localhost:2000/api/v1/login/github/callback`
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=https://www.palomino-bestbuyclone-proyect.xyz/api/v1/login/github/callback`
     res.redirect(url)
 })
 
